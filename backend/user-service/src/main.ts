6f590968b96557550e64810aa5a42ed34b552762
Feliciano Franco <f64franco@gmail.com>
2025-04-01 20:56:49 -0600

    feat: User Service CRUD

--- backend/user-service/src/main.ts
+++ backend/user-service/src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Service')
-    .setDescription('The service API description')
+    .setTitle('User Service (Gestión de Usuarios)')
+    .setDescription('Información de usuario, roles y reputación')
     .setVersion('1.0')
     //.addTag('tag')
     .build();
---
+++ backend/user-service/src/users/dto/update-user.dto.ts
@@ -0,0 +1,20 @@
+import { PartialType } from '@nestjs/swagger';
+import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty()
+  nombre_completo: string;
+
+  @ApiProperty()
+  correo: string;
+
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password_hash: string;
+
+  @ApiProperty()
+  rol: string;
+}
