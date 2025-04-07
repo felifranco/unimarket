fa72fa62a35ad9869ec45a5eb89dc13a29817c37
Feliciano Franco <f64franco@gmail.com>
2025-04-07 13:24:55 -0600

    feat: CSS Styles

--- frontend/unimarket/src/main.tsx
+++ frontend/unimarket/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "select2/dist/js/select2.min.js";
 import App from "./App.tsx";
+import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
